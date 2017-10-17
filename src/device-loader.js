8aee67b14661c80ee63df91e0366f2d3350200c0
lucaszw <lucas.zeer@gmail.com>
2017-10-17 11:49:42 -0400

    move device loader ui plugin to microdrop async

--- src/device-loader.js
+++ src/device-loader.js
@@ -1,11 +1,18 @@
+const Backbone = require('Backbone');
+const yo = require('yo-yo');
+const MicrodropAsync = require("@microdrop/async");
+
 class DeviceLoader extends UIPlugin {
   constructor(elem, focusTracker) {
     super(elem, focusTracker, "DeviceLoader");
-    this.controls = this.Controls();
-    this.listen();
+    this.microdrop = new MicrodropAsync();
+    this.model = new Backbone.Model();
+    this.render();
   }
   listen() {
+    this.onStateMsg("device-model", "device", this.onDeviceSet.bind(this));
     this.bindTriggerMsg("device-model", "load-device", "send-file");
+    this.model.on("change", this.render.bind(this));
   }
   get controls() {return this._controls}
   set controls(controls) {
@@ -13,30 +20,24 @@ class DeviceLoader extends UIPlugin {
     this.element.appendChild(controls);
     this._controls = controls;
   }
-  loadFile(file) {
-    console.log("load file called");
-    const reader = new FileReader();
-    reader.onload = () => {
-      this.trigger("send-file", this.wrapData(null, this.File(file,reader)))}
-    reader.readAsText(file);
+  async openFile() {
+    const response = await this.microdrop.device.loadFromFilePrompt();
+    return response;
   }
-  openFile() {
-    const input = D("<input type='file'/>");
-    input.on("change", (e) => this.loadFile(input.el.files[0]));
-    input.click();
+  onDeviceSet(payload) {
+    const data = JSON.parse(payload);
+    console.log(data);
+    this.model.set("filename", data.svg_filepath);
+
   }
-  File(file, reader) {
-    return {name: file.name, file: reader.result};
-  }
-  Controls() {
-    // Textfield:
-    const uploadTextfield = $("<input type='text' disabled />");
-    const uploadButton = $("<button>Upload</button>");
-    const container = $("<div></div");
-    uploadButton.on("click", this.openFile.bind(this));
-    container.append(uploadTextfield);
-    container.append(uploadButton);
-    return container[0];
+  render() {
+    const node = yo`
+    <div>
+      <input disabled type="text" value="${this.model.get("filename")}" />
+      <button class="btn btn-secondary btn-sm" onclick=${this.openFile.bind(this)}>Upload</button>
+    </div>`;
+    this.element.innerHTML = "";
+    this.element.appendChild(node);
   }
   // ** Static Methods **
   static position() {
